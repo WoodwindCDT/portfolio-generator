a378e5f0bcdb7f48a4127f74566967f9273a8191
Ender <woodwind.turbeville@gmail.com>
2020-09-23 20:20:10 -0500

    Added new generate HTML file structure and added comments

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// Global Const/Var
+// Const fs required by node to enable writeFile
+const fs = require('fs');
+// Specifies to require constant module.export to (local Path)
+// No Module.export is on this file, so create that on the new local Path file
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// Getting data from terminal in process event
+// Slice = 2, Capturing 2 expected arguments
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+// Translating data from profileDataArgs 
+// into two different variables
+const [name, github] = profileDataArgs;
+
+
+// Creates write file
+fs.writeFile('index.html', generatePage(name, github), err => {
+    // Error Handling
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
